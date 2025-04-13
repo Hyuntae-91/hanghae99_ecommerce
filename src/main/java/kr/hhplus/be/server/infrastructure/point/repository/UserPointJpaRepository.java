3610bec6074a9a903642ff90e8c62e74d92178c0
Hyuntae <dev.hyuntae@gmail.com>
2025-04-13 21:47:04 +0900

    rollback. 4주차 관련 코드 삭제

--- src/main/java/kr/hhplus/be/server/infrastructure/point/repository/UserPointJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.hhplus.be.server.infrastructure.point.repository;
-
-import kr.hhplus.be.server.domain.point.model.UserPoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPointJpaRepository extends JpaRepository<UserPoint, Long> {
-}
