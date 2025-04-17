Hyuntae-91/ecommerce-practice

refactor: 쿠폰 코드 리팩토링 및 UnitTest 코드 수정 #22

src/main/java/kr/ecommerce/be/server/domain/coupon/repository/CouponIssueRepository.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.coupon;
+package kr.ecommerce.be.server.domain.coupon.repository;
 
 import kr.ecommerce.be.server.domain.coupon.model.CouponIssue;
 
@@ -8,4 +8,5 @@ public interface CouponIssueRepository {
     CouponIssue findById(Long couponIssueId);
     CouponIssue save(CouponIssue couponIssue);
     List<CouponIssue> findByUserId(Long userId);
+    List<CouponIssue> findUsableByUserId(Long userId);
 }

src/main/java/kr/ecommerce/be/server/domain/coupon/repository/CouponRepository.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.coupon;
+package kr.ecommerce.be.server.domain.coupon.repository;
 
 import kr.ecommerce.be.server.domain.coupon.model.Coupon;
 

src/main/java/kr/ecommerce/be/server/interfaces/api/coupon/dto/CouponListResponse.java
@@ -1,6 +1,6 @@
 package kr.ecommerce.be.server.interfaces.api.coupon.dto;
 
-import kr.ecommerce.be.server.domain.coupon.dto.GetCouponsServiceResponse;
+import kr.ecommerce.be.server.domain.coupon.dto.response.GetCouponsServiceResponse;
 
 import java.util.List;
 

src/test/java/kr/ecommerce/be/server/domain/coupon/service/dto/ApplyCouponDiscountServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.ecommerce.be.server.domain.coupon.service.dto;
 
-import kr.ecommerce.be.server.domain.coupon.dto.ApplyCouponDiscountServiceRequest;
+import kr.ecommerce.be.server.domain.coupon.dto.request.ApplyCouponDiscountServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

src/test/java/kr/ecommerce/be/server/domain/coupon/service/dto/GetCouponIssueServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.ecommerce.be.server.domain.coupon.service.dto;
 
-import kr.ecommerce.be.server.domain.coupon.dto.GetCouponIssueServiceRequest;
+import kr.ecommerce.be.server.domain.coupon.dto.request.GetCouponIssueServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

src/test/java/kr/ecommerce/be/server/domain/coupon/service/dto/IssueNewCouponServiceResponseTest.java
@@ -1,7 +1,7 @@
 package kr.ecommerce.be.server.domain.coupon.service.dto;
 
 
-import kr.ecommerce.be.server.domain.coupon.dto.IssueNewCouponServiceResponse;
+import kr.ecommerce.be.server.domain.coupon.dto.response.IssueNewCouponServiceResponse;
 import kr.ecommerce.be.server.domain.coupon.model.Coupon;
 import kr.ecommerce.be.server.domain.coupon.model.CouponIssue;
 import kr.ecommerce.be.server.domain.coupon.model.CouponType;
@@ -30,7 +30,7 @@ class IssueNewCouponServiceResponseTest {
         CouponIssue issue = CouponIssue.builder()
                 .id(1L)
                 .userId(1L)
-                .coupon(coupon)
+                .couponId(1L)
                 .state(0)
                 .startAt("2025-04-11T10:00:00")
                 .endAt("2025-04-18T10:00:00")
@@ -38,7 +38,7 @@ class IssueNewCouponServiceResponseTest {
                 .updatedAt("2025-04-11T10:00:00")
                 .build();
 
-        IssueNewCouponServiceResponse response = IssueNewCouponServiceResponse.from(issue);
+        IssueNewCouponServiceResponse response = IssueNewCouponServiceResponse.from(issue, coupon);
 
         assertThat(response.couponId()).isEqualTo(1L);
         assertThat(response.type()).isEqualTo("FIXED");

src/test/java/kr/ecommerce/be/server/domain/coupon/service/model/CouponIssueTest.java
@@ -1,9 +1,10 @@
 package kr.ecommerce.be.server.domain.coupon.service.model;
 
-import kr.ecommerce.be.server.domain.coupon.dto.CouponIssueDto;
+import kr.ecommerce.be.server.domain.coupon.dto.response.CouponIssueDto;
 import kr.ecommerce.be.server.domain.coupon.model.Coupon;
 import kr.ecommerce.be.server.domain.coupon.model.CouponIssue;
 import kr.ecommerce.be.server.domain.coupon.model.CouponType;
+import kr.ecommerce.be.server.exception.custom.InvalidCouponUseException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +15,8 @@ class CouponIssueTest {
     @DisplayName("성공: FIXED 쿠폰의 할인 계산")
     void calculateFinalPrice_fixed() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        long result = issue.calculateFinalPrice(5000L);
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        long result = issue.calculateFinalPrice(5000L, coupon);
         assertThat(result).isEqualTo(4000L);
     }
 
@@ -23,8 +24,8 @@ class CouponIssueTest {
     @DisplayName("성공: PERCENT 쿠폰의 할인 계산")
     void calculateFinalPrice_percent() {
         Coupon coupon = new Coupon(2L, CouponType.PERCENT, "10% 할인", 10, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        long result = issue.calculateFinalPrice(5000L);
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        long result = issue.calculateFinalPrice(5000L, coupon);
         assertThat(result).isEqualTo(4500L);
     }
 
@@ -32,8 +33,8 @@ class CouponIssueTest {
     @DisplayName("성공: 할인액이 원금보다 클 경우 최대 할인은 원금까지")
     void calculateFinalPrice_cannot_exceed_original_price() {
         Coupon coupon = new Coupon(3L, CouponType.FIXED, "Big 할인", 99999, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        long result = issue.calculateFinalPrice(3000L);
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        long result = issue.calculateFinalPrice(3000L, coupon);
         assertThat(result).isEqualTo(0L);
     }
 
@@ -41,7 +42,7 @@ class CouponIssueTest {
     @DisplayName("성공: 사용 가능 상태 검증 통과")
     void validateUsable_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         assertThatCode(issue::validateUsable).doesNotThrowAnyException();
     }
 
@@ -49,9 +50,9 @@ class CouponIssueTest {
     @DisplayName("실패: 쿠폰 상태가 0이 아닌 경우 예외 발생")
     void validateUsable_invalid_state() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 1, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 1, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         assertThatThrownBy(issue::validateUsable)
-                .isInstanceOf(IllegalStateException.class)
+                .isInstanceOf(InvalidCouponUseException.class)
                 .hasMessageContaining("사용할 수 없는 쿠폰");
     }
 
@@ -59,7 +60,7 @@ class CouponIssueTest {
     @DisplayName("성공: 쿠폰 사용 처리")
     void markUsed_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         issue.markUsed();
         assertThat(issue.getState()).isEqualTo(1);
     }
@@ -68,7 +69,7 @@ class CouponIssueTest {
     @DisplayName("성공: 쿠폰 상태 업데이트")
     void updateState_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         issue.updateState(-1);
         assertThat(issue.getState()).isEqualTo(-1);
     }
@@ -77,8 +78,8 @@ class CouponIssueTest {
     @DisplayName("성공: toDto 동작 확인")
     void toDto_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        CouponIssueDto dto = issue.toDto();
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssueDto dto = issue.toDto(coupon);
         assertThat(dto).isNotNull();
         assertThat(dto.couponId()).isEqualTo(1L);
         assertThat(dto.discount()).isEqualTo(1000);

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponIssueRepository.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.coupon;
+package kr.hhplus.be.server.domain.coupon.repository;
 
 import kr.hhplus.be.server.domain.coupon.model.CouponIssue;
 
@@ -8,4 +8,5 @@ public interface CouponIssueRepository {
     CouponIssue findById(Long couponIssueId);
     CouponIssue save(CouponIssue couponIssue);
     List<CouponIssue> findByUserId(Long userId);
+    List<CouponIssue> findUsableByUserId(Long userId);
 }

src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.coupon;
+package kr.hhplus.be.server.domain.coupon.repository;
 
 import kr.hhplus.be.server.domain.coupon.model.Coupon;
 

src/main/java/kr/hhplus/be/server/interfaces/api/coupon/dto/CouponListResponse.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.interfaces.api.coupon.dto;
 
-import kr.hhplus.be.server.domain.coupon.dto.GetCouponsServiceResponse;
+import kr.hhplus.be.server.domain.coupon.dto.response.GetCouponsServiceResponse;
 
 import java.util.List;
 

src/test/java/kr/hhplus/be/server/domain/coupon/service/dto/ApplyCouponDiscountServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.coupon.service.dto;
 
-import kr.hhplus.be.server.domain.coupon.dto.ApplyCouponDiscountServiceRequest;
+import kr.hhplus.be.server.domain.coupon.dto.request.ApplyCouponDiscountServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

src/test/java/kr/hhplus/be/server/domain/coupon/service/dto/GetCouponIssueServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.coupon.service.dto;
 
-import kr.hhplus.be.server.domain.coupon.dto.GetCouponIssueServiceRequest;
+import kr.hhplus.be.server.domain.coupon.dto.request.GetCouponIssueServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

src/test/java/kr/hhplus/be/server/domain/coupon/service/dto/IssueNewCouponServiceResponseTest.java
@@ -1,7 +1,7 @@
 package kr.hhplus.be.server.domain.coupon.service.dto;
 
 
-import kr.hhplus.be.server.domain.coupon.dto.IssueNewCouponServiceResponse;
+import kr.hhplus.be.server.domain.coupon.dto.response.IssueNewCouponServiceResponse;
 import kr.hhplus.be.server.domain.coupon.model.Coupon;
 import kr.hhplus.be.server.domain.coupon.model.CouponIssue;
 import kr.hhplus.be.server.domain.coupon.model.CouponType;
@@ -30,7 +30,7 @@ class IssueNewCouponServiceResponseTest {
         CouponIssue issue = CouponIssue.builder()
                 .id(1L)
                 .userId(1L)
-                .coupon(coupon)
+                .couponId(1L)
                 .state(0)
                 .startAt("2025-04-11T10:00:00")
                 .endAt("2025-04-18T10:00:00")
@@ -38,7 +38,7 @@ class IssueNewCouponServiceResponseTest {
                 .updatedAt("2025-04-11T10:00:00")
                 .build();
 
-        IssueNewCouponServiceResponse response = IssueNewCouponServiceResponse.from(issue);
+        IssueNewCouponServiceResponse response = IssueNewCouponServiceResponse.from(issue, coupon);
 
         assertThat(response.couponId()).isEqualTo(1L);
         assertThat(response.type()).isEqualTo("FIXED");

src/test/java/kr/hhplus/be/server/domain/coupon/service/model/CouponIssueTest.java
@@ -1,9 +1,10 @@
 package kr.hhplus.be.server.domain.coupon.service.model;
 
-import kr.hhplus.be.server.domain.coupon.dto.CouponIssueDto;
+import kr.hhplus.be.server.domain.coupon.dto.response.CouponIssueDto;
 import kr.hhplus.be.server.domain.coupon.model.Coupon;
 import kr.hhplus.be.server.domain.coupon.model.CouponIssue;
 import kr.hhplus.be.server.domain.coupon.model.CouponType;
+import kr.hhplus.be.server.exception.custom.InvalidCouponUseException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +15,8 @@ class CouponIssueTest {
     @DisplayName("성공: FIXED 쿠폰의 할인 계산")
     void calculateFinalPrice_fixed() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        long result = issue.calculateFinalPrice(5000L);
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        long result = issue.calculateFinalPrice(5000L, coupon);
         assertThat(result).isEqualTo(4000L);
     }
 
@@ -23,8 +24,8 @@ class CouponIssueTest {
     @DisplayName("성공: PERCENT 쿠폰의 할인 계산")
     void calculateFinalPrice_percent() {
         Coupon coupon = new Coupon(2L, CouponType.PERCENT, "10% 할인", 10, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        long result = issue.calculateFinalPrice(5000L);
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        long result = issue.calculateFinalPrice(5000L, coupon);
         assertThat(result).isEqualTo(4500L);
     }
 
@@ -32,8 +33,8 @@ class CouponIssueTest {
     @DisplayName("성공: 할인액이 원금보다 클 경우 최대 할인은 원금까지")
     void calculateFinalPrice_cannot_exceed_original_price() {
         Coupon coupon = new Coupon(3L, CouponType.FIXED, "Big 할인", 99999, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        long result = issue.calculateFinalPrice(3000L);
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        long result = issue.calculateFinalPrice(3000L, coupon);
         assertThat(result).isEqualTo(0L);
     }
 
@@ -41,7 +42,7 @@ class CouponIssueTest {
     @DisplayName("성공: 사용 가능 상태 검증 통과")
     void validateUsable_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         assertThatCode(issue::validateUsable).doesNotThrowAnyException();
     }
 
@@ -49,9 +50,9 @@ class CouponIssueTest {
     @DisplayName("실패: 쿠폰 상태가 0이 아닌 경우 예외 발생")
     void validateUsable_invalid_state() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 1, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 1, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         assertThatThrownBy(issue::validateUsable)
-                .isInstanceOf(IllegalStateException.class)
+                .isInstanceOf(InvalidCouponUseException.class)
                 .hasMessageContaining("사용할 수 없는 쿠폰");
     }
 
@@ -59,7 +60,7 @@ class CouponIssueTest {
     @DisplayName("성공: 쿠폰 사용 처리")
     void markUsed_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         issue.markUsed();
         assertThat(issue.getState()).isEqualTo(1);
     }
@@ -68,7 +69,7 @@ class CouponIssueTest {
     @DisplayName("성공: 쿠폰 상태 업데이트")
     void updateState_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
         issue.updateState(-1);
         assertThat(issue.getState()).isEqualTo(-1);
     }
@@ -77,8 +78,8 @@ class CouponIssueTest {
     @DisplayName("성공: toDto 동작 확인")
     void toDto_success() {
         Coupon coupon = new Coupon(1L, CouponType.FIXED, "desc", 1000, 10, 0, 7, "2024-04-11T00:00:00", "2024-04-11T00:00:00");
-        CouponIssue issue = new CouponIssue(1L, 1L, coupon, 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
-        CouponIssueDto dto = issue.toDto();
+        CouponIssue issue = new CouponIssue(1L, 1L, coupon.getId(), 0, "2024-01-01T00:00:00", "2099-01-01T00:00:00", "2024-01-01T00:00:00", "2024-01-01T00:00:00");
+        CouponIssueDto dto = issue.toDto(coupon);
         assertThat(dto).isNotNull();
         assertThat(dto.couponId()).isEqualTo(1L);
         assertThat(dto.discount()).isEqualTo(1000);