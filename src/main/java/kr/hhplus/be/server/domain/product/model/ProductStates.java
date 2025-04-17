Hyuntae-91/ecommerce-practice

refactor: Product 기능 리팩토링 및 UnitTest 코드 수정 #19

src/main/java/kr/ecommerce/be/server/domain/product/model/ProductStates.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.product;
+package kr.ecommerce.be.server.domain.product.model;
 
 public enum ProductStates {
     DELETED(-1),

src/main/java/kr/ecommerce/be/server/domain/product/repository/ProductRepository.java
@@ -1,11 +1,9 @@
-package kr.ecommerce.be.server.domain.product;
+package kr.ecommerce.be.server.domain.product.repository;
 
 import kr.ecommerce.be.server.domain.product.model.Product;
-import kr.ecommerce.be.server.infrastructure.product.dto.GetProductsRepositoryRequestDto;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepository {
 

src/test/java/kr/ecommerce/be/server/domain/product/service/dto/request/ProductListServiceRequestTest.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.product.service.dto;
+package kr.ecommerce.be.server.domain.product.service.dto.request;
 
 import kr.ecommerce.be.server.domain.product.dto.request.ProductListServiceRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -41,5 +41,21 @@ class ProductListServiceRequestTest {
     void sortIsBlank_throwsException() {
         assertThatThrownBy(() -> new ProductListServiceRequest(1, 10, "  "))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("실패: sort가 null이면 예외 발생")
+    void nullSort_throwsException() {
+        assertThatThrownBy(() -> new ProductListServiceRequest(1, 10, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("sort 필드는 null이거나 빈 값이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("실패: sort가 빈 문자열이면 예외 발생")
+    void blankSort_throwsException() {
+        assertThatThrownBy(() -> new ProductListServiceRequest(1, 10, " "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("sort 필드는 null이거나 빈 값이 될 수 없습니다.");
     }
 }

src/test/java/kr/ecommerce/be/server/domain/product/service/dto/response/ProductOptionDtoTest.java
@@ -1,0 +1,85 @@
+package kr.ecommerce.be.server.domain.product.service.dto.response;
+
+import kr.ecommerce.be.server.domain.product.dto.response.ProductOptionResponse;
+import kr.ecommerce.be.server.interfaces.api.product.dto.response.ProductOptionDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ProductOptionDtoTest {
+
+    @Test
+    @DisplayName("성공: 유효한 값을 가진 ProductOptionDto 생성")
+    void validProductOptionDto() {
+        // given
+        Long optionId = 1L;
+        int size = 10;
+        int stock = 5;
+
+        // when
+        ProductOptionDto dto = new ProductOptionDto(optionId, size, stock);
+
+        // then
+        assertThat(dto.optionId()).isEqualTo(optionId);
+        assertThat(dto.size()).isEqualTo(size);
+        assertThat(dto.stock()).isEqualTo(stock);
+    }
+
+    @Test
+    @DisplayName("실패: optionId가 1보다 작은 경우 예외 발생")
+    void invalidOptionId() {
+        // given
+        Long optionId = 0L; // Invalid optionId
+        int size = 10;
+        int stock = 5;
+
+        // then
+        assertThatThrownBy(() -> new ProductOptionDto(optionId, size, stock))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("optionId는 1 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("실패: size가 음수일 경우 예외 발생")
+    void invalidSize() {
+        // given
+        Long optionId = 1L;
+        int size = -5; // Invalid size
+        int stock = 5;
+
+        // then
+        assertThatThrownBy(() -> new ProductOptionDto(optionId, size, stock))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("size는 0 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("실패: stock이 음수일 경우 예외 발생")
+    void invalidStock() {
+        // given
+        Long optionId = 1L;
+        int size = 10;
+        int stock = -3; // Invalid stock
+
+        // then
+        assertThatThrownBy(() -> new ProductOptionDto(optionId, size, stock))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("stock은 0 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("성공: from 메소드로 ProductOptionResponse에서 DTO 변환")
+    void fromMethodTest() {
+        // given
+        ProductOptionResponse response = new ProductOptionResponse(1L, 10, 5);
+
+        // when
+        ProductOptionDto dto = ProductOptionDto.from(response);
+
+        // then
+        assertThat(dto.optionId()).isEqualTo(1L);
+        assertThat(dto.size()).isEqualTo(10);
+        assertThat(dto.stock()).isEqualTo(5);
+    }
+}

src/test/java/kr/ecommerce/be/server/domain/product/service/dto/response/ProductOptionKeyDtoTest.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.product.service.dto;
+package kr.ecommerce.be.server.domain.product.service.dto.response;
 
 import kr.ecommerce.be.server.domain.product.dto.request.ProductOptionKeyDto;
 import org.junit.jupiter.api.DisplayName;
@@ -50,4 +50,21 @@ class ProductOptionKeyDtoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("optionId는 1 이상이어야 합니다.");
     }
+
+    @Test
+    @DisplayName("실패: itemId가 null이면 예외 발생")
+    void fail_when_itemId_is_null() {
+        assertThatThrownBy(() -> new ProductOptionKeyDto(1L, 2L, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("itemId는 1 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("실패: itemId가 1 미만이면 예외 발생")
+    void fail_when_itemId_less_than_1() {
+        assertThatThrownBy(() -> new ProductOptionKeyDto(1L, 2L, 0L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("itemId는 1 이상이어야 합니다.");
+    }
+
 }

src/test/java/kr/ecommerce/be/server/domain/product/service/dto/response/ProductOptionResponseTest.java
@@ -1,5 +1,6 @@
-package kr.ecommerce.be.server.domain.product.service.dto;
+package kr.ecommerce.be.server.domain.product.service.dto.response;
 
+import kr.ecommerce.be.server.domain.order.model.OrderOption;
 import kr.ecommerce.be.server.domain.product.dto.response.ProductOptionResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -45,5 +46,21 @@ class ProductOptionResponseTest {
         assertThatThrownBy(() -> new ProductOptionResponse(1L, 250, -5))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("stock은 0 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("성공: OrderOption으로부터 from() 변환 정상 작동")
+    void fromOrderOption_shouldConvertCorrectly() {
+        OrderOption option = OrderOption.builder()
+                .id(100L)
+                .size(275)
+                .stockQuantity(12)
+                .build();
+
+        ProductOptionResponse response = ProductOptionResponse.from(option);
+
+        assertThat(response.optionId()).isEqualTo(100L);
+        assertThat(response.size()).isEqualTo(275);
+        assertThat(response.stock()).isEqualTo(12);
     }
 }

src/test/java/kr/ecommerce/be/server/domain/product/service/dto/response/ProductServiceResponseTest.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.product.service.dto;
+package kr.ecommerce.be.server.domain.product.service.dto.response;
 
 import kr.ecommerce.be.server.domain.product.dto.response.ProductOptionResponse;
 import kr.ecommerce.be.server.domain.product.dto.response.ProductServiceResponse;
@@ -66,9 +66,17 @@ class ProductServiceResponseTest {
     }
 
     @Test
-    @DisplayName("실패: options가 null이면 예외 발생")
-    void nullOptions_throwsException() {
-        assertThatThrownBy(() -> new ProductServiceResponse(1L, "상품", 1000L, 1, "2025-04-01", null))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("성공: withOptions로 새로운 옵션 리스트 적용")
+    void withOptions_setsOptionsCorrectly() {
+        ProductServiceResponse base = new ProductServiceResponse(1L, "상품", 1000L, 1, "2025-04-01", List.of());
+        List<ProductOptionResponse> newOptions = List.of(
+                new ProductOptionResponse(2L, 260, 5)
+        );
+
+        ProductServiceResponse updated = base.withOptions(newOptions);
+        assertThat(updated.options()).hasSize(1);
+        assertThat(updated.options().get(0).optionId()).isEqualTo(2L);
     }
+
+
 }

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/domain/product/model/ProductStates.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.product;
+package kr.hhplus.be.server.domain.product.model;
 
 public enum ProductStates {
     DELETED(-1),

src/main/java/kr/hhplus/be/server/domain/product/repository/ProductRepository.java
@@ -1,11 +1,9 @@
-package kr.hhplus.be.server.domain.product;
+package kr.hhplus.be.server.domain.product.repository;
 
 import kr.hhplus.be.server.domain.product.model.Product;
-import kr.hhplus.be.server.infrastructure.product.dto.GetProductsRepositoryRequestDto;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepository {
 

src/test/java/kr/hhplus/be/server/domain/product/service/dto/request/ProductListServiceRequestTest.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.product.service.dto;
+package kr.hhplus.be.server.domain.product.service.dto.request;
 
 import kr.hhplus.be.server.domain.product.dto.request.ProductListServiceRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -41,5 +41,21 @@ class ProductListServiceRequestTest {
     void sortIsBlank_throwsException() {
         assertThatThrownBy(() -> new ProductListServiceRequest(1, 10, "  "))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("실패: sort가 null이면 예외 발생")
+    void nullSort_throwsException() {
+        assertThatThrownBy(() -> new ProductListServiceRequest(1, 10, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("sort 필드는 null이거나 빈 값이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("실패: sort가 빈 문자열이면 예외 발생")
+    void blankSort_throwsException() {
+        assertThatThrownBy(() -> new ProductListServiceRequest(1, 10, " "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("sort 필드는 null이거나 빈 값이 될 수 없습니다.");
     }
 }

src/test/java/kr/hhplus/be/server/domain/product/service/dto/response/ProductOptionDtoTest.java
@@ -1,0 +1,85 @@
+package kr.hhplus.be.server.domain.product.service.dto.response;
+
+import kr.hhplus.be.server.domain.product.dto.response.ProductOptionResponse;
+import kr.hhplus.be.server.interfaces.api.product.dto.response.ProductOptionDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ProductOptionDtoTest {
+
+    @Test
+    @DisplayName("성공: 유효한 값을 가진 ProductOptionDto 생성")
+    void validProductOptionDto() {
+        // given
+        Long optionId = 1L;
+        int size = 10;
+        int stock = 5;
+
+        // when
+        ProductOptionDto dto = new ProductOptionDto(optionId, size, stock);
+
+        // then
+        assertThat(dto.optionId()).isEqualTo(optionId);
+        assertThat(dto.size()).isEqualTo(size);
+        assertThat(dto.stock()).isEqualTo(stock);
+    }
+
+    @Test
+    @DisplayName("실패: optionId가 1보다 작은 경우 예외 발생")
+    void invalidOptionId() {
+        // given
+        Long optionId = 0L; // Invalid optionId
+        int size = 10;
+        int stock = 5;
+
+        // then
+        assertThatThrownBy(() -> new ProductOptionDto(optionId, size, stock))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("optionId는 1 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("실패: size가 음수일 경우 예외 발생")
+    void invalidSize() {
+        // given
+        Long optionId = 1L;
+        int size = -5; // Invalid size
+        int stock = 5;
+
+        // then
+        assertThatThrownBy(() -> new ProductOptionDto(optionId, size, stock))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("size는 0 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("실패: stock이 음수일 경우 예외 발생")
+    void invalidStock() {
+        // given
+        Long optionId = 1L;
+        int size = 10;
+        int stock = -3; // Invalid stock
+
+        // then
+        assertThatThrownBy(() -> new ProductOptionDto(optionId, size, stock))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("stock은 0 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("성공: from 메소드로 ProductOptionResponse에서 DTO 변환")
+    void fromMethodTest() {
+        // given
+        ProductOptionResponse response = new ProductOptionResponse(1L, 10, 5);
+
+        // when
+        ProductOptionDto dto = ProductOptionDto.from(response);
+
+        // then
+        assertThat(dto.optionId()).isEqualTo(1L);
+        assertThat(dto.size()).isEqualTo(10);
+        assertThat(dto.stock()).isEqualTo(5);
+    }
+}

src/test/java/kr/hhplus/be/server/domain/product/service/dto/response/ProductOptionKeyDtoTest.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.product.service.dto;
+package kr.hhplus.be.server.domain.product.service.dto.response;
 
 import kr.hhplus.be.server.domain.product.dto.request.ProductOptionKeyDto;
 import org.junit.jupiter.api.DisplayName;
@@ -50,4 +50,21 @@ class ProductOptionKeyDtoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("optionId는 1 이상이어야 합니다.");
     }
+
+    @Test
+    @DisplayName("실패: itemId가 null이면 예외 발생")
+    void fail_when_itemId_is_null() {
+        assertThatThrownBy(() -> new ProductOptionKeyDto(1L, 2L, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("itemId는 1 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("실패: itemId가 1 미만이면 예외 발생")
+    void fail_when_itemId_less_than_1() {
+        assertThatThrownBy(() -> new ProductOptionKeyDto(1L, 2L, 0L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("itemId는 1 이상이어야 합니다.");
+    }
+
 }

src/test/java/kr/hhplus/be/server/domain/product/service/dto/response/ProductOptionResponseTest.java
@@ -1,5 +1,6 @@
-package kr.hhplus.be.server.domain.product.service.dto;
+package kr.hhplus.be.server.domain.product.service.dto.response;
 
+import kr.hhplus.be.server.domain.order.model.OrderOption;
 import kr.hhplus.be.server.domain.product.dto.response.ProductOptionResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -45,5 +46,21 @@ class ProductOptionResponseTest {
         assertThatThrownBy(() -> new ProductOptionResponse(1L, 250, -5))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("stock은 0 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("성공: OrderOption으로부터 from() 변환 정상 작동")
+    void fromOrderOption_shouldConvertCorrectly() {
+        OrderOption option = OrderOption.builder()
+                .id(100L)
+                .size(275)
+                .stockQuantity(12)
+                .build();
+
+        ProductOptionResponse response = ProductOptionResponse.from(option);
+
+        assertThat(response.optionId()).isEqualTo(100L);
+        assertThat(response.size()).isEqualTo(275);
+        assertThat(response.stock()).isEqualTo(12);
     }
 }

src/test/java/kr/hhplus/be/server/domain/product/service/dto/response/ProductServiceResponseTest.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.product.service.dto;
+package kr.hhplus.be.server.domain.product.service.dto.response;
 
 import kr.hhplus.be.server.domain.product.dto.response.ProductOptionResponse;
 import kr.hhplus.be.server.domain.product.dto.response.ProductServiceResponse;
@@ -66,9 +66,17 @@ class ProductServiceResponseTest {
     }
 
     @Test
-    @DisplayName("실패: options가 null이면 예외 발생")
-    void nullOptions_throwsException() {
-        assertThatThrownBy(() -> new ProductServiceResponse(1L, "상품", 1000L, 1, "2025-04-01", null))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("성공: withOptions로 새로운 옵션 리스트 적용")
+    void withOptions_setsOptionsCorrectly() {
+        ProductServiceResponse base = new ProductServiceResponse(1L, "상품", 1000L, 1, "2025-04-01", List.of());
+        List<ProductOptionResponse> newOptions = List.of(
+                new ProductOptionResponse(2L, 260, 5)
+        );
+
+        ProductServiceResponse updated = base.withOptions(newOptions);
+        assertThat(updated.options()).hasSize(1);
+        assertThat(updated.options().get(0).optionId()).isEqualTo(2L);
     }
+
+
 }