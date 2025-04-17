Hyuntae-91/ecommerce-practice

feat: Payment Infrastructure 코드 개발 및 EtoE 테스트코드 추가

src/main/java/kr/ecommerce/be/server/infrastructure/payment/PaymentRepositoryImpl.java
@@ -1,0 +1,31 @@
+package kr.ecommerce.be.server.infrastructure.payment;
+
+import kr.ecommerce.be.server.domain.payment.repository.PaymentRepository;
+import kr.ecommerce.be.server.domain.payment.model.Payment;
+import kr.ecommerce.be.server.exception.custom.ResourceNotFoundException;
+import kr.ecommerce.be.server.infrastructure.payment.repository.PaymentJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+    private final PaymentJpaRepository paymentJpaRepository;
+
+    @Override
+    public Payment save(Payment payment) {
+        return paymentJpaRepository.save(payment);
+    }
+
+    @Override
+    public Payment findById(Long id) {
+        return paymentJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Payment not found"));
+    }
+
+    @Override
+    public void delete(Long id) {
+        paymentJpaRepository.deleteById(id);
+    }
+}

src/main/java/kr/ecommerce/be/server/infrastructure/payment/repository/PaymentJpaRepository.java
@@ -1,0 +1,10 @@
+package kr.ecommerce.be.server.infrastructure.payment.repository;
+
+import kr.ecommerce.be.server.domain.payment.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
+
+}

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/infrastructure/payment/PaymentRepositoryImpl.java
@@ -1,0 +1,31 @@
+package kr.hhplus.be.server.infrastructure.payment;
+
+import kr.hhplus.be.server.domain.payment.repository.PaymentRepository;
+import kr.hhplus.be.server.domain.payment.model.Payment;
+import kr.hhplus.be.server.exception.custom.ResourceNotFoundException;
+import kr.hhplus.be.server.infrastructure.payment.repository.PaymentJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+    private final PaymentJpaRepository paymentJpaRepository;
+
+    @Override
+    public Payment save(Payment payment) {
+        return paymentJpaRepository.save(payment);
+    }
+
+    @Override
+    public Payment findById(Long id) {
+        return paymentJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Payment not found"));
+    }
+
+    @Override
+    public void delete(Long id) {
+        paymentJpaRepository.deleteById(id);
+    }
+}

src/main/java/kr/hhplus/be/server/infrastructure/payment/repository/PaymentJpaRepository.java
@@ -1,0 +1,10 @@
+package kr.hhplus.be.server.infrastructure.payment.repository;
+
+import kr.hhplus.be.server.domain.payment.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
+
+}