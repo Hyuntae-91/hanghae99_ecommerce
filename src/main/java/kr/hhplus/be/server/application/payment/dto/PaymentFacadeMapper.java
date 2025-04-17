Hyuntae-91/ecommerce-practice

refactor: payment 코드 리팩토링 및 UnitTest 수정 #21

src/main/java/kr/ecommerce/be/server/application/payment/dto/PaymentFacadeMapper.java
@@ -1,8 +1,8 @@
 package kr.ecommerce.be.server.application.payment.dto;
 
 import kr.ecommerce.be.server.domain.product.dto.request.ProductOptionKeyDto;
-import kr.ecommerce.be.server.interfaces.api.payment.dto.PaymentProductDto;
-import kr.ecommerce.be.server.interfaces.api.payment.dto.PaymentRequest;
+import kr.ecommerce.be.server.interfaces.api.payment.dto.request.PaymentProductDto;
+import kr.ecommerce.be.server.interfaces.api.payment.dto.request.PaymentRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 

src/main/java/kr/ecommerce/be/server/domain/payment/dto/response/PaymentServiceResponse.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.payment.dto;
+package kr.ecommerce.be.server.domain.payment.dto.response;
 
 public record PaymentServiceResponse (
         Long paymentId,

src/main/java/kr/ecommerce/be/server/domain/payment/event/PaymentCompletedEvent.java
@@ -1,5 +1,5 @@
 package kr.ecommerce.be.server.domain.payment.event;
 
-import kr.ecommerce.be.server.domain.payment.dto.PaymentServiceResponse;
+import kr.ecommerce.be.server.domain.payment.dto.response.PaymentServiceResponse;
 
 public record PaymentCompletedEvent(PaymentServiceResponse payment) {}

src/main/java/kr/ecommerce/be/server/domain/payment/service/PaymentService.java
@@ -1,22 +1,25 @@
-package kr.ecommerce.be.server.domain.payment;
+package kr.ecommerce.be.server.domain.payment.service;
 
-import kr.ecommerce.be.server.domain.coupon.CouponIssueRepository;
+import kr.ecommerce.be.server.domain.coupon.repository.CouponIssueRepository;
 import kr.ecommerce.be.server.domain.coupon.model.CouponIssue;
-import kr.ecommerce.be.server.domain.order.OrderItemRepository;
-import kr.ecommerce.be.server.domain.order.OrderOptionRepository;
+import kr.ecommerce.be.server.domain.order.repository.OrderItemRepository;
+import kr.ecommerce.be.server.domain.order.repository.OrderOptionRepository;
 import kr.ecommerce.be.server.domain.order.model.OrderItem;
 import kr.ecommerce.be.server.domain.order.model.OrderOption;
-import kr.ecommerce.be.server.domain.payment.dto.PaymentOrderItemDto;
-import kr.ecommerce.be.server.domain.payment.dto.PaymentServiceRequest;
-import kr.ecommerce.be.server.domain.payment.dto.PaymentServiceResponse;
+import kr.ecommerce.be.server.domain.payment.dto.request.PaymentOrderItemDto;
+import kr.ecommerce.be.server.domain.payment.dto.request.PaymentServiceRequest;
+import kr.ecommerce.be.server.domain.payment.dto.response.PaymentServiceResponse;
 import kr.ecommerce.be.server.domain.payment.model.Payment;
-import kr.ecommerce.be.server.domain.point.PointRepository;
+import kr.ecommerce.be.server.domain.payment.repository.PaymentRepository;
+import kr.ecommerce.be.server.domain.point.repository.PointHistoryRepository;
+import kr.ecommerce.be.server.domain.point.repository.PointRepository;
 import kr.ecommerce.be.server.domain.point.model.PointHistoryType;
 import kr.ecommerce.be.server.domain.point.model.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,13 +29,14 @@ public class PaymentService {
     private final OrderOptionRepository orderOptionRepository;
     private final PaymentRepository paymentRepository;
     private final PointRepository userPointRepository;
+    private final PointHistoryRepository pointHistoryRepository;
     private final CouponIssueRepository couponIssueRepository;
 
     private PaymentServiceResponse toResponse(Payment payment) {
         return new PaymentServiceResponse(
                 payment.getId(),
                 payment.getOrderId(),
-                payment.getStatus(),
+                payment.getState(),
                 payment.getTotalPrice(),
                 payment.getCreatedAt()
         );
@@ -41,53 +45,61 @@ public class PaymentService {
     public PaymentServiceResponse pay(PaymentServiceRequest request) {
         Long userId = request.userId();
         long totalPrice = request.totalPrice();
-        List<OrderItem> orderItems = null;
-        Long orderId = null;
+        Long orderId = request.orderItems().get(0).orderId();
 
         try {
+            // 주문 항목 조회
             List<Long> orderItemIds = request.orderItems().stream()
                     .map(PaymentOrderItemDto::orderItemId)
                     .toList();
+            List<OrderItem> orderItems = orderItemRepository.findByIds(orderItemIds);
 
-            orderItems = orderItemRepository.findByIds(orderItemIds);
-            orderId = orderItems.get(0).getOrder().getId();
+            // optionId → OrderOption 매핑
+            var optionMap = orderItems.stream()
+                    .map(OrderItem::getOptionId)
+                    .distinct()
+                    .collect(Collectors.toMap(
+                            id -> id,
+                            orderOptionRepository::getById
+                    ));
 
+            // 재고 확인
             for (OrderItem item : orderItems) {
-                OrderOption option = orderOptionRepository.getById(item.getOptionId());
+                OrderOption option = optionMap.get(item.getOptionId());
                 option.validateEnoughStock(item.getQuantity());
             }
 
+            // 포인트 검증
             UserPoint userPoint = userPointRepository.get(userId);
             userPoint.validateUsableBalance(totalPrice);
 
+            // 재고 차감
             for (OrderItem item : orderItems) {
-                OrderOption option = orderOptionRepository.getById(item.getOptionId());
+                OrderOption option = optionMap.get(item.getOptionId());
                 option.decreaseStock(item.getQuantity());
                 orderOptionRepository.save(option);  // TODO: 병목지점 될 수 있음. 개선 필요
             }
 
+            // 쿠폰 사용 처리
             if (request.couponIssueId() != null && request.couponIssueId() > 0) {
                 CouponIssue issue = couponIssueRepository.findById(request.couponIssueId());
                 issue.markUsed();
                 couponIssueRepository.save(issue);
             }
 
+            // 포인트 차감 및 이력 저장
             userPoint.use(totalPrice);
             userPointRepository.savePoint(userPoint);
-            userPointRepository.saveHistory(userId, totalPrice, PointHistoryType.USE);
+            pointHistoryRepository.saveHistory(userId, totalPrice, PointHistoryType.USE);
 
-            Payment payment = Payment.of(
-                    orderItems.get(0).getOrder().getId(),
-                    1,
-                    totalPrice
-            );
+            // 결제 처리
+            Payment payment = Payment.of(orderId, 1, totalPrice);
             Payment savedPayment = paymentRepository.save(payment);
             return toResponse(savedPayment);
         } catch (Exception e) {
             // 결제 실패 처리: status -1
             Payment failedPayment = Payment.of(orderId, -1, totalPrice);
             Payment savedFailed = paymentRepository.save(failedPayment);
-
             return toResponse(savedFailed);
         }
     }

src/main/java/kr/ecommerce/be/server/interfaces/api/payment/PaymentApi.java
@@ -8,9 +8,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.ecommerce.be.server.exception.ErrorResponse;
-import kr.ecommerce.be.server.interfaces.api.payment.dto.PaymentCancelResponse;
-import kr.ecommerce.be.server.interfaces.api.payment.dto.PaymentRequest;
-import kr.ecommerce.be.server.interfaces.api.payment.dto.PaymentResponse;
+import kr.ecommerce.be.server.interfaces.api.payment.dto.request.PaymentRequest;
+import kr.ecommerce.be.server.interfaces.api.payment.dto.response.PaymentResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 

src/main/java/kr/ecommerce/be/server/interfaces/api/payment/dto/request/PaymentRequest.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.interfaces.api.payment.dto;
+package kr.ecommerce.be.server.interfaces.api.payment.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
@@ -10,7 +10,8 @@ public record PaymentRequest(
 
         @Schema(description = "주문 상품 리스트")
         @NotNull(message = "상품 목록은 필수입니다.")
-        List<@Valid PaymentProductDto> products,
+        @Valid
+        List<PaymentProductDto> products,
 
         @Schema(description = "사용할 쿠폰 이슈 ID", example = "1", nullable = true)
         Long couponIssueId

src/test/java/kr/ecommerce/be/server/domain/payment/service/dto/PaymentOrderItemDtoTest.java
@@ -1,6 +1,6 @@
 package kr.ecommerce.be.server.domain.payment.service.dto;
 
-import kr.ecommerce.be.server.domain.payment.dto.PaymentOrderItemDto;
+import kr.ecommerce.be.server.domain.payment.dto.request.PaymentOrderItemDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +11,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("성공: 유효한 orderItemId와 optionId로 생성")
     void create_valid_dto() {
-        PaymentOrderItemDto dto = new PaymentOrderItemDto(1L, 2L);
+        PaymentOrderItemDto dto = new PaymentOrderItemDto(1L, 1L, 2L);
 
         assertThat(dto.orderItemId()).isEqualTo(1L);
         assertThat(dto.optionId()).isEqualTo(2L);
@@ -20,7 +20,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: orderItemId가 null일 경우")
     void fail_when_orderItemId_is_null() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(null, 1L))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, null, 1L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("orderItemId는 1 이상이어야 합니다.");
     }
@@ -28,7 +28,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: orderItemId가 1보다 작을 경우")
     void fail_when_orderItemId_less_than_1() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(0L, 1L))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 0L, 1L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("orderItemId는 1 이상이어야 합니다.");
     }
@@ -36,7 +36,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: optionId가 null일 경우")
     void fail_when_optionId_is_null() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, null))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 1L, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("optionId는 1 이상이어야 합니다.");
     }
@@ -44,7 +44,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: optionId가 1보다 작을 경우")
     void fail_when_optionId_less_than_1() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 0L))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 1L, 0L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("optionId는 1 이상이어야 합니다.");
     }

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/application/payment/dto/PaymentFacadeMapper.java
@@ -1,8 +1,8 @@
 package kr.hhplus.be.server.application.payment.dto;
 
 import kr.hhplus.be.server.domain.product.dto.request.ProductOptionKeyDto;
-import kr.hhplus.be.server.interfaces.api.payment.dto.PaymentProductDto;
-import kr.hhplus.be.server.interfaces.api.payment.dto.PaymentRequest;
+import kr.hhplus.be.server.interfaces.api.payment.dto.request.PaymentProductDto;
+import kr.hhplus.be.server.interfaces.api.payment.dto.request.PaymentRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 

src/main/java/kr/hhplus/be/server/domain/payment/dto/response/PaymentServiceResponse.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.payment.dto;
+package kr.hhplus.be.server.domain.payment.dto.response;
 
 public record PaymentServiceResponse (
         Long paymentId,

src/main/java/kr/hhplus/be/server/domain/payment/event/PaymentCompletedEvent.java
@@ -1,5 +1,5 @@
 package kr.hhplus.be.server.domain.payment.event;
 
-import kr.hhplus.be.server.domain.payment.dto.PaymentServiceResponse;
+import kr.hhplus.be.server.domain.payment.dto.response.PaymentServiceResponse;
 
 public record PaymentCompletedEvent(PaymentServiceResponse payment) {}

src/main/java/kr/hhplus/be/server/domain/payment/service/PaymentService.java
@@ -1,22 +1,25 @@
-package kr.hhplus.be.server.domain.payment;
+package kr.hhplus.be.server.domain.payment.service;
 
-import kr.hhplus.be.server.domain.coupon.CouponIssueRepository;
+import kr.hhplus.be.server.domain.coupon.repository.CouponIssueRepository;
 import kr.hhplus.be.server.domain.coupon.model.CouponIssue;
-import kr.hhplus.be.server.domain.order.OrderItemRepository;
-import kr.hhplus.be.server.domain.order.OrderOptionRepository;
+import kr.hhplus.be.server.domain.order.repository.OrderItemRepository;
+import kr.hhplus.be.server.domain.order.repository.OrderOptionRepository;
 import kr.hhplus.be.server.domain.order.model.OrderItem;
 import kr.hhplus.be.server.domain.order.model.OrderOption;
-import kr.hhplus.be.server.domain.payment.dto.PaymentOrderItemDto;
-import kr.hhplus.be.server.domain.payment.dto.PaymentServiceRequest;
-import kr.hhplus.be.server.domain.payment.dto.PaymentServiceResponse;
+import kr.hhplus.be.server.domain.payment.dto.request.PaymentOrderItemDto;
+import kr.hhplus.be.server.domain.payment.dto.request.PaymentServiceRequest;
+import kr.hhplus.be.server.domain.payment.dto.response.PaymentServiceResponse;
 import kr.hhplus.be.server.domain.payment.model.Payment;
-import kr.hhplus.be.server.domain.point.PointRepository;
+import kr.hhplus.be.server.domain.payment.repository.PaymentRepository;
+import kr.hhplus.be.server.domain.point.repository.PointHistoryRepository;
+import kr.hhplus.be.server.domain.point.repository.PointRepository;
 import kr.hhplus.be.server.domain.point.model.PointHistoryType;
 import kr.hhplus.be.server.domain.point.model.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,13 +29,14 @@ public class PaymentService {
     private final OrderOptionRepository orderOptionRepository;
     private final PaymentRepository paymentRepository;
     private final PointRepository userPointRepository;
+    private final PointHistoryRepository pointHistoryRepository;
     private final CouponIssueRepository couponIssueRepository;
 
     private PaymentServiceResponse toResponse(Payment payment) {
         return new PaymentServiceResponse(
                 payment.getId(),
                 payment.getOrderId(),
-                payment.getStatus(),
+                payment.getState(),
                 payment.getTotalPrice(),
                 payment.getCreatedAt()
         );
@@ -41,53 +45,61 @@ public class PaymentService {
     public PaymentServiceResponse pay(PaymentServiceRequest request) {
         Long userId = request.userId();
         long totalPrice = request.totalPrice();
-        List<OrderItem> orderItems = null;
-        Long orderId = null;
+        Long orderId = request.orderItems().get(0).orderId();
 
         try {
+            // 주문 항목 조회
             List<Long> orderItemIds = request.orderItems().stream()
                     .map(PaymentOrderItemDto::orderItemId)
                     .toList();
+            List<OrderItem> orderItems = orderItemRepository.findByIds(orderItemIds);
 
-            orderItems = orderItemRepository.findByIds(orderItemIds);
-            orderId = orderItems.get(0).getOrder().getId();
+            // optionId → OrderOption 매핑
+            var optionMap = orderItems.stream()
+                    .map(OrderItem::getOptionId)
+                    .distinct()
+                    .collect(Collectors.toMap(
+                            id -> id,
+                            orderOptionRepository::getById
+                    ));
 
+            // 재고 확인
             for (OrderItem item : orderItems) {
-                OrderOption option = orderOptionRepository.getById(item.getOptionId());
+                OrderOption option = optionMap.get(item.getOptionId());
                 option.validateEnoughStock(item.getQuantity());
             }
 
+            // 포인트 검증
             UserPoint userPoint = userPointRepository.get(userId);
             userPoint.validateUsableBalance(totalPrice);
 
+            // 재고 차감
             for (OrderItem item : orderItems) {
-                OrderOption option = orderOptionRepository.getById(item.getOptionId());
+                OrderOption option = optionMap.get(item.getOptionId());
                 option.decreaseStock(item.getQuantity());
                 orderOptionRepository.save(option);  // TODO: 병목지점 될 수 있음. 개선 필요
             }
 
+            // 쿠폰 사용 처리
             if (request.couponIssueId() != null && request.couponIssueId() > 0) {
                 CouponIssue issue = couponIssueRepository.findById(request.couponIssueId());
                 issue.markUsed();
                 couponIssueRepository.save(issue);
             }
 
+            // 포인트 차감 및 이력 저장
             userPoint.use(totalPrice);
             userPointRepository.savePoint(userPoint);
-            userPointRepository.saveHistory(userId, totalPrice, PointHistoryType.USE);
+            pointHistoryRepository.saveHistory(userId, totalPrice, PointHistoryType.USE);
 
-            Payment payment = Payment.of(
-                    orderItems.get(0).getOrder().getId(),
-                    1,
-                    totalPrice
-            );
+            // 결제 처리
+            Payment payment = Payment.of(orderId, 1, totalPrice);
             Payment savedPayment = paymentRepository.save(payment);
             return toResponse(savedPayment);
         } catch (Exception e) {
             // 결제 실패 처리: status -1
             Payment failedPayment = Payment.of(orderId, -1, totalPrice);
             Payment savedFailed = paymentRepository.save(failedPayment);
-
             return toResponse(savedFailed);
         }
     }

src/main/java/kr/hhplus/be/server/interfaces/api/payment/PaymentApi.java
@@ -8,9 +8,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.exception.ErrorResponse;
-import kr.hhplus.be.server.interfaces.api.payment.dto.PaymentCancelResponse;
-import kr.hhplus.be.server.interfaces.api.payment.dto.PaymentRequest;
-import kr.hhplus.be.server.interfaces.api.payment.dto.PaymentResponse;
+import kr.hhplus.be.server.interfaces.api.payment.dto.request.PaymentRequest;
+import kr.hhplus.be.server.interfaces.api.payment.dto.response.PaymentResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 

src/main/java/kr/hhplus/be/server/interfaces/api/payment/dto/request/PaymentRequest.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.interfaces.api.payment.dto;
+package kr.hhplus.be.server.interfaces.api.payment.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
@@ -10,7 +10,8 @@ public record PaymentRequest(
 
         @Schema(description = "주문 상품 리스트")
         @NotNull(message = "상품 목록은 필수입니다.")
-        List<@Valid PaymentProductDto> products,
+        @Valid
+        List<PaymentProductDto> products,
 
         @Schema(description = "사용할 쿠폰 이슈 ID", example = "1", nullable = true)
         Long couponIssueId

src/test/java/kr/hhplus/be/server/domain/payment/service/dto/PaymentOrderItemDtoTest.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.payment.service.dto;
 
-import kr.hhplus.be.server.domain.payment.dto.PaymentOrderItemDto;
+import kr.hhplus.be.server.domain.payment.dto.request.PaymentOrderItemDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +11,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("성공: 유효한 orderItemId와 optionId로 생성")
     void create_valid_dto() {
-        PaymentOrderItemDto dto = new PaymentOrderItemDto(1L, 2L);
+        PaymentOrderItemDto dto = new PaymentOrderItemDto(1L, 1L, 2L);
 
         assertThat(dto.orderItemId()).isEqualTo(1L);
         assertThat(dto.optionId()).isEqualTo(2L);
@@ -20,7 +20,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: orderItemId가 null일 경우")
     void fail_when_orderItemId_is_null() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(null, 1L))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, null, 1L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("orderItemId는 1 이상이어야 합니다.");
     }
@@ -28,7 +28,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: orderItemId가 1보다 작을 경우")
     void fail_when_orderItemId_less_than_1() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(0L, 1L))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 0L, 1L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("orderItemId는 1 이상이어야 합니다.");
     }
@@ -36,7 +36,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: optionId가 null일 경우")
     void fail_when_optionId_is_null() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, null))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 1L, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("optionId는 1 이상이어야 합니다.");
     }
@@ -44,7 +44,7 @@ class PaymentOrderItemDtoTest {
     @Test
     @DisplayName("실패: optionId가 1보다 작을 경우")
     void fail_when_optionId_less_than_1() {
-        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 0L))
+        assertThatThrownBy(() -> new PaymentOrderItemDto(1L, 1L, 0L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("optionId는 1 이상이어야 합니다.");
     }