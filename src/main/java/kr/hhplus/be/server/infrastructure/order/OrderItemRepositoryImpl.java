Hyuntae-91/ecommerce-practice

feat: 주문 Infrastructure 코드 개발 및 EtoE 테스트코드 추가

src/main/java/kr/ecommerce/be/server/infrastructure/order/OrderItemRepositoryImpl.java
@@ -1,0 +1,48 @@
+package kr.ecommerce.be.server.infrastructure.order;
+
+import kr.ecommerce.be.server.domain.order.repository.OrderItemRepository;
+import kr.ecommerce.be.server.domain.order.model.OrderItem;
+import kr.ecommerce.be.server.exception.custom.ResourceNotFoundException;
+import kr.ecommerce.be.server.infrastructure.order.repository.OrderItemJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderItemRepositoryImpl implements OrderItemRepository {
+
+    private final OrderItemJpaRepository orderItemJpaRepository;
+
+    @Override
+    public OrderItem findById(Long id) {
+        return orderItemJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("OrderItem not found: " + id));
+    }
+
+    @Override
+    public OrderItem save(OrderItem orderItem) {
+        return orderItemJpaRepository.save(orderItem);
+    }
+
+    @Override
+    public List<OrderItem> findByIds(List<Long> orderItemIds) {
+        return orderItemJpaRepository.findAllById(orderItemIds);
+    }
+
+    @Override
+    public List<OrderItem> findAllByOrderId(Long orderId) {
+        return orderItemJpaRepository.findAllByOrderId(orderId);
+    }
+
+    @Override
+    public List<OrderItem> findCartByUserId(Long userId) {
+        return orderItemJpaRepository.findCartByUserId(userId);
+    }
+
+    @Override
+    public List<OrderItem> saveAll(List<OrderItem> orderItems) {
+        return orderItemJpaRepository.saveAll(orderItems);
+    }
+}

src/main/java/kr/ecommerce/be/server/infrastructure/order/OrderOptionRepositoryImpl.java
@@ -1,0 +1,33 @@
+package kr.ecommerce.be.server.infrastructure.order;
+
+import kr.ecommerce.be.server.domain.order.repository.OrderOptionRepository;
+import kr.ecommerce.be.server.domain.order.model.OrderOption;
+import kr.ecommerce.be.server.exception.custom.ResourceNotFoundException;
+import kr.ecommerce.be.server.infrastructure.order.repository.OrderOptionJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderOptionRepositoryImpl  implements OrderOptionRepository {
+
+    private final OrderOptionJpaRepository orderOptionJpaRepository;
+
+    @Override
+    public OrderOption getById(Long id) {
+        return orderOptionJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("OrderOption not found. id = " + id));
+    }
+
+    @Override
+    public List<OrderOption> findByProductId(Long productId) {
+        return orderOptionJpaRepository.findByProductId(productId);
+    }
+
+    @Override
+    public OrderOption save(OrderOption orderOption) {
+        return orderOptionJpaRepository.save(orderOption);
+    }
+}

src/main/java/kr/ecommerce/be/server/infrastructure/order/OrderRepositoryImpl.java
@@ -1,0 +1,26 @@
+package kr.ecommerce.be.server.infrastructure.order;
+
+import kr.ecommerce.be.server.exception.custom.ResourceNotFoundException;
+import kr.ecommerce.be.server.domain.order.repository.OrderRepository;
+import kr.ecommerce.be.server.domain.order.model.Order;
+import kr.ecommerce.be.server.infrastructure.order.repository.OrderJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private final OrderJpaRepository orderJpaRepository;
+
+    @Override
+    public Order save(Order order) {
+        return orderJpaRepository.save(order);
+    }
+
+    @Override
+    public Order getById(Long id) {
+        return orderJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Order Not Found"));
+    }
+}

src/main/java/kr/ecommerce/be/server/infrastructure/order/repository/OrderItemJpaRepository.java
@@ -1,0 +1,13 @@
+package kr.ecommerce.be.server.infrastructure.order.repository;
+
+import kr.ecommerce.be.server.domain.order.model.OrderItem;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderItemJpaRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrderId(Long orderId);
+
+    List<OrderItem> findCartByUserId(Long userId);
+}

src/main/java/kr/ecommerce/be/server/infrastructure/order/repository/OrderJpaRepository.java
@@ -1,0 +1,7 @@
+package kr.ecommerce.be.server.infrastructure.order.repository;
+
+import kr.ecommerce.be.server.domain.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+}

src/main/java/kr/ecommerce/be/server/infrastructure/order/repository/OrderOptionJpaRepository.java
@@ -1,0 +1,10 @@
+package kr.ecommerce.be.server.infrastructure.order.repository;
+
+import kr.ecommerce.be.server.domain.order.model.OrderOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderOptionJpaRepository extends JpaRepository<OrderOption, Long> {
+    List<OrderOption> findByProductId(Long productId);
+}

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/infrastructure/order/OrderItemRepositoryImpl.java
@@ -1,0 +1,48 @@
+package kr.hhplus.be.server.infrastructure.order;
+
+import kr.hhplus.be.server.domain.order.repository.OrderItemRepository;
+import kr.hhplus.be.server.domain.order.model.OrderItem;
+import kr.hhplus.be.server.exception.custom.ResourceNotFoundException;
+import kr.hhplus.be.server.infrastructure.order.repository.OrderItemJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderItemRepositoryImpl implements OrderItemRepository {
+
+    private final OrderItemJpaRepository orderItemJpaRepository;
+
+    @Override
+    public OrderItem findById(Long id) {
+        return orderItemJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("OrderItem not found: " + id));
+    }
+
+    @Override
+    public OrderItem save(OrderItem orderItem) {
+        return orderItemJpaRepository.save(orderItem);
+    }
+
+    @Override
+    public List<OrderItem> findByIds(List<Long> orderItemIds) {
+        return orderItemJpaRepository.findAllById(orderItemIds);
+    }
+
+    @Override
+    public List<OrderItem> findAllByOrderId(Long orderId) {
+        return orderItemJpaRepository.findAllByOrderId(orderId);
+    }
+
+    @Override
+    public List<OrderItem> findCartByUserId(Long userId) {
+        return orderItemJpaRepository.findCartByUserId(userId);
+    }
+
+    @Override
+    public List<OrderItem> saveAll(List<OrderItem> orderItems) {
+        return orderItemJpaRepository.saveAll(orderItems);
+    }
+}

src/main/java/kr/hhplus/be/server/infrastructure/order/OrderOptionRepositoryImpl.java
@@ -1,0 +1,33 @@
+package kr.hhplus.be.server.infrastructure.order;
+
+import kr.hhplus.be.server.domain.order.repository.OrderOptionRepository;
+import kr.hhplus.be.server.domain.order.model.OrderOption;
+import kr.hhplus.be.server.exception.custom.ResourceNotFoundException;
+import kr.hhplus.be.server.infrastructure.order.repository.OrderOptionJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderOptionRepositoryImpl  implements OrderOptionRepository {
+
+    private final OrderOptionJpaRepository orderOptionJpaRepository;
+
+    @Override
+    public OrderOption getById(Long id) {
+        return orderOptionJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("OrderOption not found. id = " + id));
+    }
+
+    @Override
+    public List<OrderOption> findByProductId(Long productId) {
+        return orderOptionJpaRepository.findByProductId(productId);
+    }
+
+    @Override
+    public OrderOption save(OrderOption orderOption) {
+        return orderOptionJpaRepository.save(orderOption);
+    }
+}

src/main/java/kr/hhplus/be/server/infrastructure/order/OrderRepositoryImpl.java
@@ -1,0 +1,26 @@
+package kr.hhplus.be.server.infrastructure.order;
+
+import kr.hhplus.be.server.exception.custom.ResourceNotFoundException;
+import kr.hhplus.be.server.domain.order.repository.OrderRepository;
+import kr.hhplus.be.server.domain.order.model.Order;
+import kr.hhplus.be.server.infrastructure.order.repository.OrderJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private final OrderJpaRepository orderJpaRepository;
+
+    @Override
+    public Order save(Order order) {
+        return orderJpaRepository.save(order);
+    }
+
+    @Override
+    public Order getById(Long id) {
+        return orderJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Order Not Found"));
+    }
+}

src/main/java/kr/hhplus/be/server/infrastructure/order/repository/OrderItemJpaRepository.java
@@ -1,0 +1,13 @@
+package kr.hhplus.be.server.infrastructure.order.repository;
+
+import kr.hhplus.be.server.domain.order.model.OrderItem;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderItemJpaRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrderId(Long orderId);
+
+    List<OrderItem> findCartByUserId(Long userId);
+}

src/main/java/kr/hhplus/be/server/infrastructure/order/repository/OrderJpaRepository.java
@@ -1,0 +1,7 @@
+package kr.hhplus.be.server.infrastructure.order.repository;
+
+import kr.hhplus.be.server.domain.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+}

src/main/java/kr/hhplus/be/server/infrastructure/order/repository/OrderOptionJpaRepository.java
@@ -1,0 +1,10 @@
+package kr.hhplus.be.server.infrastructure.order.repository;
+
+import kr.hhplus.be.server.domain.order.model.OrderOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderOptionJpaRepository extends JpaRepository<OrderOption, Long> {
+    List<OrderOption> findByProductId(Long productId);
+}