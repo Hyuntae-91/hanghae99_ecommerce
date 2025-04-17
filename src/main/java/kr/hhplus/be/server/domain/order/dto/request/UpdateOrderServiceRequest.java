Hyuntae-91/ecommerce-practice

refactor: 주문 관련 코드 리팩토링 및 UnitTest 코드 수정 #20

src/main/java/kr/ecommerce/be/server/domain/order/dto/request/UpdateOrderServiceRequest.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.order.dto;
+package kr.ecommerce.be.server.domain.order.dto.request;
 
 public record UpdateOrderServiceRequest(
         Long orderId,

src/main/java/kr/ecommerce/be/server/domain/order/dto/response/CreateOrderServiceResponse.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.order.dto;
+package kr.ecommerce.be.server.domain.order.dto.response;
 
 public record CreateOrderServiceResponse(
         Long orderId

src/main/java/kr/ecommerce/be/server/domain/order/model/OrderItem.java
@@ -1,7 +1,7 @@
 package kr.ecommerce.be.server.domain.order.model;
 
 import jakarta.persistence.*;
-import kr.ecommerce.be.server.domain.order.dto.CartItemResponse;
+import kr.ecommerce.be.server.domain.order.dto.response.CartItemResponse;
 import kr.ecommerce.be.server.domain.product.model.Product;
 import lombok.*;
 
@@ -19,6 +19,7 @@ public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
     private Long id;
 
     private Long userId;
@@ -26,6 +27,10 @@ public class OrderItem {
     @Column(name = "product_id")
     private Long productId;
 
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "order_option_id")
     private Long optionId;
 
     private Long eachPrice;
@@ -36,35 +41,12 @@ public class OrderItem {
 
     private String updatedAt;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", insertable = false, updatable = false)
-    private Product product;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", insertable = false, updatable = false)
-    private Order order;
-
-    @Setter
-    @ManyToOne(fetch = FetchType.LAZY)
-    private OrderOption orderOption;
-
     public Long calculateTotalPrice() {
         return eachPrice * quantity;
     }
 
-    public OrderOption getOption(List<OrderOption> options) {
-        return options.stream()
-                .filter(option -> option.getId().equals(optionId)) // optionId로 필터링
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("옵션이 존재하지 않습니다."));
-    }
-
-    public int getStock(List<OrderOption> options) {
-        return getOption(options).getStockQuantity();
-    }
-
-    public int getSize(List<OrderOption> options) {
-        return getOption(options).getSize();
+    public void applyOrderId(Long orderId) {
+        this.orderId = orderId;
     }
 
     public static OrderItem of(
@@ -86,20 +68,14 @@ public class OrderItem {
                 .build();
     }
 
-    public CartItemResponse toCartItemResponse() {
-        if (orderOption == null) {
-            throw new IllegalStateException("OrderOption이 초기화되지 않았습니다.");
-        }
-        int stock = orderOption.getStockQuantity();
-        int size = orderOption.getSize();
-
+    public CartItemResponse toCartItemResponse(OrderOption option) {
         return new CartItemResponse(
-                productId,
-                quantity,
-                optionId,
-                eachPrice,
-                stock,
-                size
+                this.productId,
+                this.quantity,
+                this.optionId,
+                this.eachPrice,
+                option.getStockQuantity(),
+                option.getSize()
         );
     }
 }

src/main/java/kr/ecommerce/be/server/domain/order/repository/OrderRepository.java
@@ -1,4 +1,4 @@
-package kr.ecommerce.be.server.domain.order;
+package kr.ecommerce.be.server.domain.order.repository;
 
 import kr.ecommerce.be.server.domain.order.model.Order;
 

src/main/java/kr/ecommerce/be/server/domain/order/service/OrderService.java
@@ -1,8 +1,17 @@
-package kr.ecommerce.be.server.domain.order;
+package kr.ecommerce.be.server.domain.order.service;
 
-import kr.ecommerce.be.server.domain.order.dto.*;
+import kr.ecommerce.be.server.domain.order.dto.request.*;
+import kr.ecommerce.be.server.domain.order.dto.response.AddCartServiceResponse;
+import kr.ecommerce.be.server.domain.order.dto.response.CartItemResponse;
+import kr.ecommerce.be.server.domain.order.dto.response.CartItemServiceResponse;
+import kr.ecommerce.be.server.domain.order.dto.response.CreateOrderServiceResponse;
 import kr.ecommerce.be.server.domain.order.model.Order;
 import kr.ecommerce.be.server.domain.order.model.OrderItem;
+import kr.ecommerce.be.server.domain.order.model.OrderOption;
+import kr.ecommerce.be.server.domain.order.repository.OrderItemRepository;
+import kr.ecommerce.be.server.domain.order.repository.OrderOptionRepository;
+import kr.ecommerce.be.server.domain.order.repository.OrderRepository;
+import kr.ecommerce.be.server.exception.custom.OrderItemNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,23 +26,28 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
+    private final OrderOptionRepository orderOptionRepository;
 
     public CartItemServiceResponse getCart(GetCartServiceRequest request) {
         List<OrderItem> cartItems = orderItemRepository.findCartByUserId(request.userId());
 
         List<CartItemResponse> cartList = cartItems.stream()
-                .map(OrderItem::toCartItemResponse)
+                .map(item -> item.toCartItemResponse(
+                        orderOptionRepository.getById(item.getOptionId())
+                ))
                 .toList();
 
         long totalPrice = cartList.stream()
-                .mapToLong(CartItemResponse::eachPrice)
+                .mapToLong(cartItem -> cartItem.eachPrice() * cartItem.quantity())
                 .sum();
 
         return new CartItemServiceResponse(cartList, totalPrice);
     }
 
     public AddCartServiceResponse addCartService(AddCartServiceRequest request) {
-        OrderItem item = OrderItem.of(
+        OrderOption option = orderOptionRepository.getById(request.optionId());
+        option.validateEnoughStock(request.quantity());
+        OrderItem orderItem = OrderItem.of(
                 request.userId(),
                 request.productId(),
                 request.optionId(),
@@ -41,33 +55,33 @@ public class OrderService {
                 request.quantity()
         );
 
-        orderItemRepository.save(item);
+        orderItemRepository.save(orderItem);
 
         List<OrderItem> cartItems = orderItemRepository.findCartByUserId(request.userId());
 
         List<CartItemResponse> cartList = cartItems.stream()
-                .map(OrderItem::toCartItemResponse)
+                .map(item -> item.toCartItemResponse(
+                        orderOptionRepository.getById(item.getOptionId())
+                ))
                 .toList();
 
         long totalPrice = cartList.stream()
-                .mapToLong(CartItemResponse::eachPrice)
+                .mapToLong(cartItem -> cartItem.eachPrice() * cartItem.quantity())
                 .sum();
 
         return new AddCartServiceResponse(cartList, totalPrice);
     }
 
     public CreateOrderServiceResponse createOrder(CreateOrderServiceRequest requestDto) {
-        Order order = Order.of(requestDto.userId(), requestDto.couponIssueId(), 0L, 0);
-        Order saved = orderRepository.save(order);
-        if (saved.getOrderItems().isEmpty()) {
-            throw new IllegalStateException("주문 항목이 존재하지 않습니다.");
+        List<OrderItem> cartItems = orderItemRepository.findCartByUserId(requestDto.userId());
+        if (cartItems.isEmpty()) {
+            throw new OrderItemNotFoundException("주문 항목이 존재하지 않습니다.");
         }
-        Long orderId = saved.getId();
 
         Map<Long, Integer> quantityMap = requestDto.items().stream()
                 .collect(Collectors.toMap(CreateOrderItemDto::itemId, CreateOrderItemDto::quantity));
 
-        List<OrderItem> updatedItems = saved.getOrderItems().stream()
+        List<OrderItem> updatedItems = cartItems.stream()
                 .filter(item -> quantityMap.containsKey(item.getId()))
                 .map(item -> OrderItem.of(
                         item.getUserId(),
@@ -78,8 +92,15 @@ public class OrderService {
                 ))
                 .toList();
 
-        orderItemRepository.saveAll(updatedItems);
-        return new CreateOrderServiceResponse(orderId);
+        Order order = Order.of(requestDto.userId(), requestDto.couponIssueId(), 0L, 0);
+        Order saved = orderRepository.save(order);
+        if (saved == null) {
+            throw new OrderItemNotFoundException("주문 저장 실패");
+        }
+        updatedItems.forEach(item -> item.applyOrderId(saved.getId()));
+        List<OrderItem> newUpdatedItems = orderItemRepository.saveAll(updatedItems);
+
+        return new CreateOrderServiceResponse(saved.getId());
     }
 
     public void updateTotalPrice(UpdateOrderServiceRequest request) {

src/test/java/kr/ecommerce/be/server/domain/order/service/dto/AddCartServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.ecommerce.be.server.domain.order.service.dto;
 
-import kr.ecommerce.be.server.domain.order.dto.AddCartServiceRequest;
+import kr.ecommerce.be.server.domain.order.dto.request.AddCartServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

src/test/java/kr/ecommerce/be/server/domain/order/service/dto/GetCartServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.ecommerce.be.server.domain.order.service.dto;
 
-import kr.ecommerce.be.server.domain.order.dto.GetCartServiceRequest;
+import kr.ecommerce.be.server.domain.order.dto.request.GetCartServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/domain/order/dto/request/UpdateOrderServiceRequest.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.order.dto;
+package kr.hhplus.be.server.domain.order.dto.request;
 
 public record UpdateOrderServiceRequest(
         Long orderId,

src/main/java/kr/hhplus/be/server/domain/order/dto/response/CreateOrderServiceResponse.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.order.dto;
+package kr.hhplus.be.server.domain.order.dto.response;
 
 public record CreateOrderServiceResponse(
         Long orderId

src/main/java/kr/hhplus/be/server/domain/order/model/OrderItem.java
@@ -1,7 +1,7 @@
 package kr.hhplus.be.server.domain.order.model;
 
 import jakarta.persistence.*;
-import kr.hhplus.be.server.domain.order.dto.CartItemResponse;
+import kr.hhplus.be.server.domain.order.dto.response.CartItemResponse;
 import kr.hhplus.be.server.domain.product.model.Product;
 import lombok.*;
 
@@ -19,6 +19,7 @@ public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
     private Long id;
 
     private Long userId;
@@ -26,6 +27,10 @@ public class OrderItem {
     @Column(name = "product_id")
     private Long productId;
 
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "order_option_id")
     private Long optionId;
 
     private Long eachPrice;
@@ -36,35 +41,12 @@ public class OrderItem {
 
     private String updatedAt;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", insertable = false, updatable = false)
-    private Product product;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", insertable = false, updatable = false)
-    private Order order;
-
-    @Setter
-    @ManyToOne(fetch = FetchType.LAZY)
-    private OrderOption orderOption;
-
     public Long calculateTotalPrice() {
         return eachPrice * quantity;
     }
 
-    public OrderOption getOption(List<OrderOption> options) {
-        return options.stream()
-                .filter(option -> option.getId().equals(optionId)) // optionId로 필터링
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("옵션이 존재하지 않습니다."));
-    }
-
-    public int getStock(List<OrderOption> options) {
-        return getOption(options).getStockQuantity();
-    }
-
-    public int getSize(List<OrderOption> options) {
-        return getOption(options).getSize();
+    public void applyOrderId(Long orderId) {
+        this.orderId = orderId;
     }
 
     public static OrderItem of(
@@ -86,20 +68,14 @@ public class OrderItem {
                 .build();
     }
 
-    public CartItemResponse toCartItemResponse() {
-        if (orderOption == null) {
-            throw new IllegalStateException("OrderOption이 초기화되지 않았습니다.");
-        }
-        int stock = orderOption.getStockQuantity();
-        int size = orderOption.getSize();
-
+    public CartItemResponse toCartItemResponse(OrderOption option) {
         return new CartItemResponse(
-                productId,
-                quantity,
-                optionId,
-                eachPrice,
-                stock,
-                size
+                this.productId,
+                this.quantity,
+                this.optionId,
+                this.eachPrice,
+                option.getStockQuantity(),
+                option.getSize()
         );
     }
 }

src/main/java/kr/hhplus/be/server/domain/order/repository/OrderRepository.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.domain.order;
+package kr.hhplus.be.server.domain.order.repository;
 
 import kr.hhplus.be.server.domain.order.model.Order;
 

src/main/java/kr/hhplus/be/server/domain/order/service/OrderService.java
@@ -1,8 +1,17 @@
-package kr.hhplus.be.server.domain.order;
+package kr.hhplus.be.server.domain.order.service;
 
-import kr.hhplus.be.server.domain.order.dto.*;
+import kr.hhplus.be.server.domain.order.dto.request.*;
+import kr.hhplus.be.server.domain.order.dto.response.AddCartServiceResponse;
+import kr.hhplus.be.server.domain.order.dto.response.CartItemResponse;
+import kr.hhplus.be.server.domain.order.dto.response.CartItemServiceResponse;
+import kr.hhplus.be.server.domain.order.dto.response.CreateOrderServiceResponse;
 import kr.hhplus.be.server.domain.order.model.Order;
 import kr.hhplus.be.server.domain.order.model.OrderItem;
+import kr.hhplus.be.server.domain.order.model.OrderOption;
+import kr.hhplus.be.server.domain.order.repository.OrderItemRepository;
+import kr.hhplus.be.server.domain.order.repository.OrderOptionRepository;
+import kr.hhplus.be.server.domain.order.repository.OrderRepository;
+import kr.hhplus.be.server.exception.custom.OrderItemNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,23 +26,28 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
+    private final OrderOptionRepository orderOptionRepository;
 
     public CartItemServiceResponse getCart(GetCartServiceRequest request) {
         List<OrderItem> cartItems = orderItemRepository.findCartByUserId(request.userId());
 
         List<CartItemResponse> cartList = cartItems.stream()
-                .map(OrderItem::toCartItemResponse)
+                .map(item -> item.toCartItemResponse(
+                        orderOptionRepository.getById(item.getOptionId())
+                ))
                 .toList();
 
         long totalPrice = cartList.stream()
-                .mapToLong(CartItemResponse::eachPrice)
+                .mapToLong(cartItem -> cartItem.eachPrice() * cartItem.quantity())
                 .sum();
 
         return new CartItemServiceResponse(cartList, totalPrice);
     }
 
     public AddCartServiceResponse addCartService(AddCartServiceRequest request) {
-        OrderItem item = OrderItem.of(
+        OrderOption option = orderOptionRepository.getById(request.optionId());
+        option.validateEnoughStock(request.quantity());
+        OrderItem orderItem = OrderItem.of(
                 request.userId(),
                 request.productId(),
                 request.optionId(),
@@ -41,33 +55,33 @@ public class OrderService {
                 request.quantity()
         );
 
-        orderItemRepository.save(item);
+        orderItemRepository.save(orderItem);
 
         List<OrderItem> cartItems = orderItemRepository.findCartByUserId(request.userId());
 
         List<CartItemResponse> cartList = cartItems.stream()
-                .map(OrderItem::toCartItemResponse)
+                .map(item -> item.toCartItemResponse(
+                        orderOptionRepository.getById(item.getOptionId())
+                ))
                 .toList();
 
         long totalPrice = cartList.stream()
-                .mapToLong(CartItemResponse::eachPrice)
+                .mapToLong(cartItem -> cartItem.eachPrice() * cartItem.quantity())
                 .sum();
 
         return new AddCartServiceResponse(cartList, totalPrice);
     }
 
     public CreateOrderServiceResponse createOrder(CreateOrderServiceRequest requestDto) {
-        Order order = Order.of(requestDto.userId(), requestDto.couponIssueId(), 0L, 0);
-        Order saved = orderRepository.save(order);
-        if (saved.getOrderItems().isEmpty()) {
-            throw new IllegalStateException("주문 항목이 존재하지 않습니다.");
+        List<OrderItem> cartItems = orderItemRepository.findCartByUserId(requestDto.userId());
+        if (cartItems.isEmpty()) {
+            throw new OrderItemNotFoundException("주문 항목이 존재하지 않습니다.");
         }
-        Long orderId = saved.getId();
 
         Map<Long, Integer> quantityMap = requestDto.items().stream()
                 .collect(Collectors.toMap(CreateOrderItemDto::itemId, CreateOrderItemDto::quantity));
 
-        List<OrderItem> updatedItems = saved.getOrderItems().stream()
+        List<OrderItem> updatedItems = cartItems.stream()
                 .filter(item -> quantityMap.containsKey(item.getId()))
                 .map(item -> OrderItem.of(
                         item.getUserId(),
@@ -78,8 +92,15 @@ public class OrderService {
                 ))
                 .toList();
 
-        orderItemRepository.saveAll(updatedItems);
-        return new CreateOrderServiceResponse(orderId);
+        Order order = Order.of(requestDto.userId(), requestDto.couponIssueId(), 0L, 0);
+        Order saved = orderRepository.save(order);
+        if (saved == null) {
+            throw new OrderItemNotFoundException("주문 저장 실패");
+        }
+        updatedItems.forEach(item -> item.applyOrderId(saved.getId()));
+        List<OrderItem> newUpdatedItems = orderItemRepository.saveAll(updatedItems);
+
+        return new CreateOrderServiceResponse(saved.getId());
     }
 
     public void updateTotalPrice(UpdateOrderServiceRequest request) {

src/test/java/kr/hhplus/be/server/domain/order/service/dto/AddCartServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.order.service.dto;
 
-import kr.hhplus.be.server.domain.order.dto.AddCartServiceRequest;
+import kr.hhplus.be.server.domain.order.dto.request.AddCartServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

src/test/java/kr/hhplus/be/server/domain/order/service/dto/GetCartServiceRequestTest.java
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.order.service.dto;
 
-import kr.hhplus.be.server.domain.order.dto.GetCartServiceRequest;
+import kr.hhplus.be.server.domain.order.dto.request.GetCartServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 