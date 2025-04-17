Hyuntae-91/ecommerce-practice

feat: Product Infrastructure 코드 개발 및 EtoE 테스트 코드 추가

src/main/java/kr/ecommerce/be/server/infrastructure/product/ProductRepositoryImpl.java
@@ -1,0 +1,52 @@
+package kr.ecommerce.be.server.infrastructure.product;
+
+import kr.ecommerce.be.server.exception.custom.ResourceNotFoundException;
+import kr.ecommerce.be.server.domain.product.repository.ProductRepository;
+import kr.ecommerce.be.server.domain.product.model.Product;
+import kr.ecommerce.be.server.infrastructure.product.repository.ProductJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final ProductJpaRepository productJpaRepository;
+
+    @Override
+    public Product findById(Long id) {
+        return productJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product Not Found"));
+    }
+
+    @Override
+    public List<Product> findByIds(List<Long> ids) {
+        return productJpaRepository.findByIdIn(ids);
+    }
+
+    @Override
+    public List<Product> findByStateNotIn(int page, int size, String sort, List<Integer> excludeStates) {
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, sort));
+        return productJpaRepository.findByStateNotIn(excludeStates, pageable).getContent();
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productJpaRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findPopularTop5() {
+        return productJpaRepository.findTop5PopularProducts(); // 가정: score 필드가 있음
+    }
+
+    @Override
+    public void recalculateBestProducts() {
+        // TODO: 추후 통계 계산 쿼리로 대체
+    }
+}

src/main/java/kr/ecommerce/be/server/infrastructure/product/repository/ProductJpaRepository.java
@@ -1,0 +1,36 @@
+package kr.ecommerce.be.server.infrastructure.product.repository;
+
+import kr.ecommerce.be.server.domain.product.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findByIdIn(List<Long> ids);
+
+    Page<Product> findByStateNotIn(List<Integer> excludeStates, Pageable pageable);
+
+    @Query(value = """
+    SELECT p.*
+    FROM product p
+    JOIN (
+        SELECT oi.product_id
+        FROM order_item oi
+        JOIN `order` o ON oi.order_id = o.id
+        WHERE o.state = 1
+        GROUP BY oi.product_id
+        ORDER BY SUM(oi.quantity) DESC
+        LIMIT 5
+    ) popular ON p.id = popular.product_id
+    """, nativeQuery = true)
+    List<Product> findTop5PopularProducts();
+
+}

src/main/java/kr/ecommerce/be/server/interfaces/api/payment/dto/request/PaymentProductDto.java
@@ -1,0 +1,33 @@
+package kr.ecommerce.be.server.interfaces.api.payment.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentProductDto(
+
+        @Schema(description = "상품 ID", example = "1")
+        @NotNull(message = "상품 ID는 필수입니다.")
+        @Min(value = 1, message = "상품 ID는 1 이상이어야 합니다.")
+        Long id,
+
+        @Schema(description = "상품명", example = "상품상세")
+        @NotNull(message = "상품명은 필수입니다.")
+        String name,
+
+        @Schema(description = "아이템 ID", example = "1")
+        @NotNull(message = "아이템 ID는 필수입니다.")
+        @Min(value = 1, message = "아이템 ID는 1 이상이어야 합니다.")
+        Long itemId,
+
+        @Schema(description = "옵션 ID", example = "1")
+        @NotNull(message = "옵션 ID는 필수입니다.")
+        @Min(value = 1, message = "옵션 ID는 1 이상이어야 합니다.")
+        Long optionId,
+
+        @Schema(description = "수량 ID", example = "1")
+        @NotNull(message = "수량 ID는 필수입니다.")
+        @Min(value = 1, message = "수량 ID는 1 이상이어야 합니다.")
+        Integer quantity
+) {
+}

src/test/java/kr/ecommerce/be/server/api/controller/product/ProductControllerTest.java
@@ -1,0 +1,213 @@
+package kr.ecommerce.be.server.api.controller.product;
+
+import kr.ecommerce.be.server.domain.order.model.Order;
+import kr.ecommerce.be.server.domain.order.model.OrderItem;
+import kr.ecommerce.be.server.domain.order.model.OrderOption;
+import kr.ecommerce.be.server.domain.order.repository.OrderOptionRepository;
+import kr.ecommerce.be.server.domain.product.model.Product;
+import kr.ecommerce.be.server.infrastructure.order.repository.OrderItemJpaRepository;
+import kr.ecommerce.be.server.infrastructure.order.repository.OrderJpaRepository;
+import kr.ecommerce.be.server.infrastructure.product.repository.ProductJpaRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductJpaRepository productJpaRepository;
+
+    @Autowired
+    private OrderJpaRepository orderJpaRepository;
+
+    @Autowired
+    private OrderItemJpaRepository orderItemJpaRepository;
+
+    @Autowired
+    private OrderOptionRepository orderOptionJpaRepository;
+
+    @Test
+    @Transactional
+    @DisplayName("성공: 상품 단건 조회")
+    void get_product_success() throws Exception {
+        // given
+        Product product = Product.builder()
+                .name("테스트 상품")
+                .price(1000L)
+                .state(1)
+                .createdAt("2025-04-15T00:00:00")
+                .updatedAt("2025-04-15T00:00:00")
+                .build();
+        Product saved = productJpaRepository.save(product);
+
+        OrderOption option = OrderOption.builder()
+                .productId(saved.getId())
+                .size(100)
+                .stockQuantity(10)
+                .createdAt("2025-04-15T00:00:00")
+                .updatedAt("2025-04-15T00:00:00")
+                .build();
+        orderOptionJpaRepository.save(option);
+
+        // when & then
+        mockMvc.perform(get("/v1/products/" + saved.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(saved.getId()))
+                .andExpect(jsonPath("$.name").value("테스트 상품"))
+                .andExpect(jsonPath("$.price").value(1000))
+                .andExpect(jsonPath("$.state").value(1))
+                .andExpect(jsonPath("$.createdAt").value("2025-04-15T00:00:00"));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("성공: 상품 리스트 조회")
+    void get_product_list_success() throws Exception {
+        // given
+        productJpaRepository.save(Product.builder()
+                .name("상품 A")
+                .price(1000L)
+                .state(1)
+                .createdAt("2025-04-15T00:00:00")
+                .updatedAt("2025-04-15T00:00:00")
+                .build());
+
+        productJpaRepository.save(Product.builder()
+                .name("상품 B")
+                .price(2000L)
+                .state(1)
+                .createdAt("2025-04-14T00:00:00")
+                .updatedAt("2025-04-14T00:00:00")
+                .build());
+
+        String payload = """
+        {
+          "page": 1,
+          "size": 10,
+          "sort": "createdAt"
+        }
+        """;
+
+        // when & then
+        mockMvc.perform(get("/v1/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").exists())
+                .andExpect(jsonPath("$[0].price").exists())
+                .andExpect(jsonPath("$[0].state").value(1));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("성공: 인기 상품 조회")
+    void get_best_products_success() throws Exception {
+        // given: 상품 2개
+        Product productA = productJpaRepository.save(Product.builder()
+                .name("인기상품 A").price(1000L).state(1)
+                .createdAt("2025-04-10T00:00:00").updatedAt("2025-04-10T00:00:00").build());
+
+        Product productB = productJpaRepository.save(Product.builder()
+                .name("인기상품 B").price(2000L).state(1)
+                .createdAt("2025-04-10T00:00:00").updatedAt("2025-04-10T00:00:00").build());
+
+        // orderOption 생성
+        OrderOption optionA = orderOptionJpaRepository.save(OrderOption.builder()
+                .productId(productA.getId())
+                .size(275)
+                .stockQuantity(10)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        OrderOption optionB = orderOptionJpaRepository.save(OrderOption.builder()
+                .productId(productB.getId())
+                .size(280)
+                .stockQuantity(5)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        // 주문 아이템 리스트 생성
+        List<OrderItem> items = List.of(
+                OrderItem.builder()
+                        .userId(1L)
+                        .productId(productA.getId())
+                        .optionId(optionA.getId())
+                        .eachPrice(1000L)
+                        .quantity(2)
+                        .createdAt("2025-04-10T00:00:00")
+                        .updatedAt("2025-04-10T00:00:00")
+                        .build(),
+                OrderItem.builder()
+                        .userId(1L)
+                        .productId(productB.getId())
+                        .optionId(optionB.getId())
+                        .eachPrice(2000L)
+                        .quantity(1)
+                        .createdAt("2025-04-10T00:00:00")
+                        .updatedAt("2025-04-10T00:00:00")
+                        .build()
+        );
+
+        // 주문 및 아이템
+        Order order = orderJpaRepository.save(Order.of(1L, null, 5000L, 1));
+
+        orderItemJpaRepository.save(OrderItem.builder()
+                .orderId(order.getId())
+                .userId(1L)
+                .productId(productA.getId())
+                .optionId(optionA.getId())
+                .eachPrice(1000L)
+                .quantity(2)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        orderItemJpaRepository.save(OrderItem.builder()
+                .orderId(order.getId())
+                .userId(1L)
+                .productId(productB.getId())
+                .optionId(optionB.getId())
+                .eachPrice(2000L)
+                .quantity(1)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        // when & then
+        mockMvc.perform(get("/v1/products/bests"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(productA.getId())) // 수량 2가 먼저
+                .andExpect(jsonPath("$[1].id").value(productB.getId())); // 수량 1이 다음
+    }
+
+    @Test
+    @DisplayName("성공: 인기 상품 통계 계산 요청")
+    void calculate_best_products_success() throws Exception {
+        mockMvc.perform(post("/v1/products/best/calculate"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+}

Hyuntae-91/hanghae99_ecommerce

src/main/java/kr/hhplus/be/server/infrastructure/product/ProductRepositoryImpl.java
@@ -1,0 +1,52 @@
+package kr.hhplus.be.server.infrastructure.product;
+
+import kr.hhplus.be.server.exception.custom.ResourceNotFoundException;
+import kr.hhplus.be.server.domain.product.repository.ProductRepository;
+import kr.hhplus.be.server.domain.product.model.Product;
+import kr.hhplus.be.server.infrastructure.product.repository.ProductJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final ProductJpaRepository productJpaRepository;
+
+    @Override
+    public Product findById(Long id) {
+        return productJpaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product Not Found"));
+    }
+
+    @Override
+    public List<Product> findByIds(List<Long> ids) {
+        return productJpaRepository.findByIdIn(ids);
+    }
+
+    @Override
+    public List<Product> findByStateNotIn(int page, int size, String sort, List<Integer> excludeStates) {
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, sort));
+        return productJpaRepository.findByStateNotIn(excludeStates, pageable).getContent();
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productJpaRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findPopularTop5() {
+        return productJpaRepository.findTop5PopularProducts(); // 가정: score 필드가 있음
+    }
+
+    @Override
+    public void recalculateBestProducts() {
+        // TODO: 추후 통계 계산 쿼리로 대체
+    }
+}

src/main/java/kr/hhplus/be/server/infrastructure/product/repository/ProductJpaRepository.java
@@ -1,0 +1,36 @@
+package kr.hhplus.be.server.infrastructure.product.repository;
+
+import kr.hhplus.be.server.domain.product.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findByIdIn(List<Long> ids);
+
+    Page<Product> findByStateNotIn(List<Integer> excludeStates, Pageable pageable);
+
+    @Query(value = """
+    SELECT p.*
+    FROM product p
+    JOIN (
+        SELECT oi.product_id
+        FROM order_item oi
+        JOIN `order` o ON oi.order_id = o.id
+        WHERE o.state = 1
+        GROUP BY oi.product_id
+        ORDER BY SUM(oi.quantity) DESC
+        LIMIT 5
+    ) popular ON p.id = popular.product_id
+    """, nativeQuery = true)
+    List<Product> findTop5PopularProducts();
+
+}

src/main/java/kr/hhplus/be/server/interfaces/api/payment/dto/request/PaymentProductDto.java
@@ -1,0 +1,33 @@
+package kr.hhplus.be.server.interfaces.api.payment.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentProductDto(
+
+        @Schema(description = "상품 ID", example = "1")
+        @NotNull(message = "상품 ID는 필수입니다.")
+        @Min(value = 1, message = "상품 ID는 1 이상이어야 합니다.")
+        Long id,
+
+        @Schema(description = "상품명", example = "상품상세")
+        @NotNull(message = "상품명은 필수입니다.")
+        String name,
+
+        @Schema(description = "아이템 ID", example = "1")
+        @NotNull(message = "아이템 ID는 필수입니다.")
+        @Min(value = 1, message = "아이템 ID는 1 이상이어야 합니다.")
+        Long itemId,
+
+        @Schema(description = "옵션 ID", example = "1")
+        @NotNull(message = "옵션 ID는 필수입니다.")
+        @Min(value = 1, message = "옵션 ID는 1 이상이어야 합니다.")
+        Long optionId,
+
+        @Schema(description = "수량 ID", example = "1")
+        @NotNull(message = "수량 ID는 필수입니다.")
+        @Min(value = 1, message = "수량 ID는 1 이상이어야 합니다.")
+        Integer quantity
+) {
+}

src/test/java/kr/hhplus/be/server/api/controller/product/ProductControllerTest.java
@@ -1,0 +1,213 @@
+package kr.hhplus.be.server.api.controller.product;
+
+import kr.hhplus.be.server.domain.order.model.Order;
+import kr.hhplus.be.server.domain.order.model.OrderItem;
+import kr.hhplus.be.server.domain.order.model.OrderOption;
+import kr.hhplus.be.server.domain.order.repository.OrderOptionRepository;
+import kr.hhplus.be.server.domain.product.model.Product;
+import kr.hhplus.be.server.infrastructure.order.repository.OrderItemJpaRepository;
+import kr.hhplus.be.server.infrastructure.order.repository.OrderJpaRepository;
+import kr.hhplus.be.server.infrastructure.product.repository.ProductJpaRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductJpaRepository productJpaRepository;
+
+    @Autowired
+    private OrderJpaRepository orderJpaRepository;
+
+    @Autowired
+    private OrderItemJpaRepository orderItemJpaRepository;
+
+    @Autowired
+    private OrderOptionRepository orderOptionJpaRepository;
+
+    @Test
+    @Transactional
+    @DisplayName("성공: 상품 단건 조회")
+    void get_product_success() throws Exception {
+        // given
+        Product product = Product.builder()
+                .name("테스트 상품")
+                .price(1000L)
+                .state(1)
+                .createdAt("2025-04-15T00:00:00")
+                .updatedAt("2025-04-15T00:00:00")
+                .build();
+        Product saved = productJpaRepository.save(product);
+
+        OrderOption option = OrderOption.builder()
+                .productId(saved.getId())
+                .size(100)
+                .stockQuantity(10)
+                .createdAt("2025-04-15T00:00:00")
+                .updatedAt("2025-04-15T00:00:00")
+                .build();
+        orderOptionJpaRepository.save(option);
+
+        // when & then
+        mockMvc.perform(get("/v1/products/" + saved.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(saved.getId()))
+                .andExpect(jsonPath("$.name").value("테스트 상품"))
+                .andExpect(jsonPath("$.price").value(1000))
+                .andExpect(jsonPath("$.state").value(1))
+                .andExpect(jsonPath("$.createdAt").value("2025-04-15T00:00:00"));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("성공: 상품 리스트 조회")
+    void get_product_list_success() throws Exception {
+        // given
+        productJpaRepository.save(Product.builder()
+                .name("상품 A")
+                .price(1000L)
+                .state(1)
+                .createdAt("2025-04-15T00:00:00")
+                .updatedAt("2025-04-15T00:00:00")
+                .build());
+
+        productJpaRepository.save(Product.builder()
+                .name("상품 B")
+                .price(2000L)
+                .state(1)
+                .createdAt("2025-04-14T00:00:00")
+                .updatedAt("2025-04-14T00:00:00")
+                .build());
+
+        String payload = """
+        {
+          "page": 1,
+          "size": 10,
+          "sort": "createdAt"
+        }
+        """;
+
+        // when & then
+        mockMvc.perform(get("/v1/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").exists())
+                .andExpect(jsonPath("$[0].price").exists())
+                .andExpect(jsonPath("$[0].state").value(1));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("성공: 인기 상품 조회")
+    void get_best_products_success() throws Exception {
+        // given: 상품 2개
+        Product productA = productJpaRepository.save(Product.builder()
+                .name("인기상품 A").price(1000L).state(1)
+                .createdAt("2025-04-10T00:00:00").updatedAt("2025-04-10T00:00:00").build());
+
+        Product productB = productJpaRepository.save(Product.builder()
+                .name("인기상품 B").price(2000L).state(1)
+                .createdAt("2025-04-10T00:00:00").updatedAt("2025-04-10T00:00:00").build());
+
+        // orderOption 생성
+        OrderOption optionA = orderOptionJpaRepository.save(OrderOption.builder()
+                .productId(productA.getId())
+                .size(275)
+                .stockQuantity(10)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        OrderOption optionB = orderOptionJpaRepository.save(OrderOption.builder()
+                .productId(productB.getId())
+                .size(280)
+                .stockQuantity(5)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        // 주문 아이템 리스트 생성
+        List<OrderItem> items = List.of(
+                OrderItem.builder()
+                        .userId(1L)
+                        .productId(productA.getId())
+                        .optionId(optionA.getId())
+                        .eachPrice(1000L)
+                        .quantity(2)
+                        .createdAt("2025-04-10T00:00:00")
+                        .updatedAt("2025-04-10T00:00:00")
+                        .build(),
+                OrderItem.builder()
+                        .userId(1L)
+                        .productId(productB.getId())
+                        .optionId(optionB.getId())
+                        .eachPrice(2000L)
+                        .quantity(1)
+                        .createdAt("2025-04-10T00:00:00")
+                        .updatedAt("2025-04-10T00:00:00")
+                        .build()
+        );
+
+        // 주문 및 아이템
+        Order order = orderJpaRepository.save(Order.of(1L, null, 5000L, 1));
+
+        orderItemJpaRepository.save(OrderItem.builder()
+                .orderId(order.getId())
+                .userId(1L)
+                .productId(productA.getId())
+                .optionId(optionA.getId())
+                .eachPrice(1000L)
+                .quantity(2)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        orderItemJpaRepository.save(OrderItem.builder()
+                .orderId(order.getId())
+                .userId(1L)
+                .productId(productB.getId())
+                .optionId(optionB.getId())
+                .eachPrice(2000L)
+                .quantity(1)
+                .createdAt("2025-04-10T00:00:00")
+                .updatedAt("2025-04-10T00:00:00")
+                .build());
+
+        // when & then
+        mockMvc.perform(get("/v1/products/bests"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(productA.getId())) // 수량 2가 먼저
+                .andExpect(jsonPath("$[1].id").value(productB.getId())); // 수량 1이 다음
+    }
+
+    @Test
+    @DisplayName("성공: 인기 상품 통계 계산 요청")
+    void calculate_best_products_success() throws Exception {
+        mockMvc.perform(post("/v1/products/best/calculate"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+}